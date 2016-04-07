71444edd09d3b79139001505255526352dc2ad15
Steve Sloka <slokas@upmc.edu>
2016-04-07 10:13:47 -0400

    Updated myevents to run from ng-service

--- adminClient/www/js/controllers.js
+++ adminClient/www/js/controllers.js
@@ -1,42 +1,42 @@
 angular.module('app.controllers', [])
-  
-.controller('myEventsCtrl', function($scope) {
 
-})
-   
-.controller('myVolunteersCtrl', function($scope) {
+    .controller('myEventsCtrl', function ($scope, EventsService) {
+        $scope.events = EventsService.events;
+    })
 
-})
-      
-.controller('loginCtrl', function($scope) {
+    .controller('myVolunteersCtrl', function ($scope) {
 
-})
-   
-.controller('modifyEventCtrl', function($scope, $stateParams) {
-    $scope.eventOverview = "you chose event: " + $stateParams.eventId; //"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-})
-   
-.controller('eventSignInCtrl', function($scope) {
+    })
 
-})
-   
-.controller('eventsignupCtrl', function($scope) {
+    .controller('loginCtrl', function ($scope) {
 
-})
-   
-.controller('volunteerDetailCtrl', function($scope) {
+    })
 
-})
-   
-.controller('manageCreditsCtrl', function($scope) {
+    .controller('modifyEventCtrl', function ($scope, $stateParams, EventsService) {
+        $scope.event = EventsService.getEvent($stateParams.eventId);
+    })
 
-})
-   
-.controller('creditDetailsCtrl', function($scope) {
+    .controller('eventSignInCtrl', function ($scope) {
 
-})
-   
-.controller('login2Ctrl', function($scope) {
+    })
 
-})
+    .controller('eventsignupCtrl', function ($scope) {
+
+    })
+
+    .controller('volunteerDetailCtrl', function ($scope) {
+
+    })
+
+    .controller('manageCreditsCtrl', function ($scope) {
+
+    })
+
+    .controller('creditDetailsCtrl', function ($scope) {
+
+    })
+
+    .controller('login2Ctrl', function ($scope) {
+
+    })
  
--- adminClient/www/js/services.js
+++ adminClient/www/js/services.js
@@ -1,10 +1,61 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+    .factory('BlankFactory', [function () {
 
-}])
+    }])
 
-.service('BlankService', [function(){
+    .service('BlankService', [function () {
 
-}]);
+    }])
+
+    .service('EventsService', function () {
+        var events = [
+            {
+                eventName: "Clean Highland Park Playground",
+                eventOverview: "desc1",
+                duration: 5,
+                notes: "fkdsldkskld s;kd;sd",
+                isFamilyFriendly: true,
+                minAge: 15,
+                volunteers: [
+                    { name: "Steve Sloka" },
+                    { name: "User 2" },
+                    { name: "User Five" }
+                ]
+            },
+            {
+                eventName: "Paint Kid's Room at St. Mary's Church",
+                eventOverview: "desc2",
+                duration: 15,
+                notes: "something else",
+                isFamilyFriendly: false,
+                minAge: 90,
+                volunteers: [
+                    { name: "User 2" },
+                    { name: "User Five" }
+                ]
+            },
+            {
+                eventName: "Make cards for Vets",
+                eventOverview: "desc3",
+                duration: 56,
+                notes: "fkyet another!!",
+                isFamilyFriendly: true,
+                minAge: 21,
+                volunteers: [
+                    { name: "User 82" },
+                    { name: "User Seven" }
+                ]
+            }
+
+        ];
+
+        return {
+            events: events,
+            getEvent: function (eventId) {
+                return events[eventId];
+            }
+        }
+
+    });
 
