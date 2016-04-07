96db798e932883a2cbaba5adf628c4cd70753c05
Steve Sloka <slokas@upmc.edu>
2016-04-07 09:19:46 -0400

    initial commit

---
+++ adminClient/www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('myEventsCtrl', function($scope) {
+
+})
+   
+.controller('myVolunteersCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('modifyEventCtrl', function($scope, $stateParams) {
+    $scope.eventOverview = "you chose event: " + $stateParams.eventId; //"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+})
+   
+.controller('eventSignInCtrl', function($scope) {
+
+})
+   
+.controller('eventsignupCtrl', function($scope) {
+
+})
+   
+.controller('volunteerDetailCtrl', function($scope) {
+
+})
+   
+.controller('manageCreditsCtrl', function($scope) {
+
+})
+   
+.controller('creditDetailsCtrl', function($scope) {
+
+})
+   
+.controller('login2Ctrl', function($scope) {
+
+})
+ 
