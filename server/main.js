670f92b5c559a6699f4651eb8d7894e5adc22892
Mike Levin <levinmr@upmc.edu>
2016-04-08 18:43:33 -0400

    Add cors headers

--- server/main.js
+++ server/main.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add CORS headers
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
